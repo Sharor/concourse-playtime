4aba7fa82a93bbd1809e2227301a36c35d1c9865
Sharor <davchristensen90@gmail.com>
2016-12-15 13:41:16 +0100

    Why is lock different if pulled from repo?

--- build/build.sh
+++ build/build.sh
@@ -14,10 +14,8 @@ ls
 git config --global user.email "johannes_christensen@msn.com"
 git config --global user.name "sharor"
 
-dotnet restore
 dotnet build
 git status
-ls ./dotnet-testfolder
 
 git add .
 git commit -m "build dotnet project"
