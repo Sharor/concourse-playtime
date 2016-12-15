1ebdaedb0ef9082126886666bba5985c58901925
Sharor <davchristensen90@gmail.com>
2016-12-15 12:46:54 +0100

    trying this workaround

--- build/restore.sh
+++ build/restore.sh
@@ -3,14 +3,13 @@
 set -e # fail fast
 set -x # print commands
 
-git clone dotnet-test
-cd dotnet-test
-
-
+git clone dotnet-test restore
+cd restore
 
 git config --global user.email "johannes_christensen@msn.com"
 git config --global user.name "sharor"
 
 dotnet restore
+
 git add .
 git commit -m "restore dotnet project" 
