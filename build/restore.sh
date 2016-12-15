b5fc6f8e971483a3a23a8856564ffbcca0420602
Sharor <davchristensen90@gmail.com>
2016-12-15 12:38:14 +0100

    fixed restore script

--- build/restore.sh
+++ build/restore.sh
@@ -1,11 +1,16 @@
-    ls
-    git clone dotnet-test
-    
-    git config --global user.email "johannes_christensen@msn.com"
-    git config --global user.name "sharor"
-   
-    dotnet restore
-    cd dotnet-test
-    ls
-    git add .
-    git commit -m "restore dotnet project" 
+#!/bin/sh
+
+set -e # fail fast
+set -x # print commands
+
+git clone dotnet-test
+cd dotnet-test
+
+
+
+git config --global user.email "johannes_christensen@msn.com"
+git config --global user.name "sharor"
+
+dotnet restore
+git add .
+git commit -m "restore dotnet project" 
