27019b48fca271ec682fdf9334103cb44b223ac1
Sharor <davchristensen90@gmail.com>
2016-12-15 11:08:27 +0100

    lets try smth

--- concourse-tutorial/create_some_files.sh
+++ concourse-tutorial/create_some_files.sh
@@ -5,5 +5,7 @@ echo "file1" > some-files/file1
 echo "file2" > some-files/file2
 echo "file3" > some-files/file3
 echo "file4" > some-files/file4
+git add . 
+git commit -m "le stuffz"
 ls
 ls some-files/*
