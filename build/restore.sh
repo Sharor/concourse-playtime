f1f88703f6eefcb0f2157d2cfe5052b5cf6b21a8
Sharor <davchristensen90@gmail.com>
2016-12-15 12:34:26 +0100

    more troubleshooting

---
+++ build/restore.sh
@@ -0,0 +1,11 @@
+    ls
+    git clone dotnet-test
+    
+    git config --global user.email "johannes_christensen@msn.com"
+    git config --global user.name "sharor"
+   
+    dotnet restore
+    cd dotnet-test
+    ls
+    git add .
+    git commit -m "restore dotnet project" 
