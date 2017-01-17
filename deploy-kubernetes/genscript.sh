2d41a8149c360f596ac44bde72882d9c274796d9
Sharor <davchristensen90@gmail.com>
2017-01-17 15:23:03 +0100

    script now has permissions, odd - need to work in the folder

--- deploy-kubernetes/genscript.sh
+++ deploy-kubernetes/genscript.sh
@@ -2,11 +2,12 @@
 
 set -e # fail fast
 set -x # print commands
+
+cd repository/deploy-kubernetes
 ls
-echo "$KUBE_CA" | base64 -d > repository/deploy-kubernetes/ca-minikube
-echo "$KUBE_KEY" | base64 -d > repository/deploy-kubernetes/client-key
-echo "$KUBE_CERT" | base64 -d > repository/deploy-kubernetes/client-cert
-ls
+echo "$KUBE_CA" | base64 -d > ca-minikube
+echo "$KUBE_KEY" | base64 -d > client-key
+echo "$KUBE_CERT" | base64 -d > client-cert
 
 kubectl --server=https://192.168.99.100:8443 --namespace=default --certificate-authority=$CA_PATH --client-key=$KEY_PATH --client-certificate=$CERT_PATH" create -f repository/svc.yml
 
