5299b88b73d80aafe04c8bf6809cb3d0e3af1b21
Sharor <davchristensen90@gmail.com>
2017-01-17 15:16:47 +0100

    bad path

--- deploy-kubernetes/genscript.sh
+++ deploy-kubernetes/genscript.sh
@@ -1,6 +1,7 @@
-echo "$KUBE_CA" | base64 -d > ca-minikube
-echo "$KUBE_KEY" | base64 -d > client-key
-echo "$KUBE_CERT" | base64 -d > client-cert
-
+ls
+echo "$KUBE_CA" | base64 -d > repository/ca-minikube
+echo "$KUBE_KEY" | base64 -d > repository/client-key
+echo "$KUBE_CERT" | base64 -d > repository/client-cert
+ls
 kubectl --server=https://192.168.99.100:8443 --namespace=default --certificate-authority=$CA_PATH --client-key=$KEY_PATH --client-certificate=$CERT_PATH" create -f repository/svc.yml
 
