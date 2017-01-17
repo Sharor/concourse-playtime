d2557e34b39f94dba157f7afbf034fcc1d102362
Sharor <davchristensen90@gmail.com>
2017-01-17 15:18:55 +0100

    certs were in deploy folder

--- deploy-kubernetes/genscript.sh
+++ deploy-kubernetes/genscript.sh
@@ -1,7 +1,6 @@
-ls
-echo "$KUBE_CA" | base64 -d > repository/ca-minikube
-echo "$KUBE_KEY" | base64 -d > repository/client-key
-echo "$KUBE_CERT" | base64 -d > repository/client-cert
+echo "$KUBE_CA" | base64 -d > repository/deploy-kubernetes/ca-minikube
+echo "$KUBE_KEY" | base64 -d > repository/deploy-kubernetes/client-key
+echo "$KUBE_CERT" | base64 -d > repository/deploy-kubernetes/client-cert
 ls
 kubectl --server=https://192.168.99.100:8443 --namespace=default --certificate-authority=$CA_PATH --client-key=$KEY_PATH --client-certificate=$CERT_PATH" create -f repository/svc.yml
 
